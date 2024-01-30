0e04b0004dbdbfa7d19f70a0a209eff31dd2d2ba
marius rousset <rousset.marius13@gmail.com>
2024-01-30 21:50:57 +0100

    feat: my alias

--- alias/custom.sh
+++ alias/custom.sh
@@ -1,17 +1,18 @@
-# alias
+#!/usr/bin/bash
 
-<!-- usefull -->
+# usefull
 
 alias hs="hstyle"
 alias cs="cstyle"
 alias v="valgrind"
 alias l="ls"
 alias cpy="xclip -selection clipboard"
+alias extract='tar -xvzf'
 
-<!-- remember alias location -->
+# remember alias location
 
 alias aliasdir="c /etc/profile.d/custom.sh"
 
-<!-- old -->
+# old
 
 alias '?'='echo $?'
--- shell/.zshrc
+++ shell/.zshrc
@@ -1,3 +1,6 @@
+# at the top
+zstyle ':omz:*' aliases no
+
 ZSH_THEME="robbyrussell"  
 CASE_SENSITIVE="true"  
 PROMPT+="%{$reset_color%}%(?..%{$fg_bold[blue]%}status:(%{$fg_bold[red]%}%?%{$fg_bold[blue]%}%)%{$reset_color%} )"
